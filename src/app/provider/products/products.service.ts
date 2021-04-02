00306e7380869b731ce22f49598bb87aee587030
Fabian Acero Garcia <acero01@gmail.com>
2021-04-02 13:58:23 -0500

    FEATURE - Adiciono mock y cambio logo

--- src/app/provider/products/products.service.ts
+++ src/app/provider/products/products.service.ts
@@ -1,17 +1,25 @@
 import { Injectable } from '@angular/core';
 
-import {HttpRequestService} from '../http-request/http-request.service';
-import {Observable} from 'rxjs';
-import {ProductServiceResponse} from '../../models/product-service-response';
+import { HttpRequestService } from '../http-request/http-request.service';
+import { Observable, of } from 'rxjs';
+import { ProductServiceResponse } from '../../models/product-service-response';
+import { productsAndServices } from "../../mocks/productsAndServices.mock";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
+  public enableMock = true;
 
-  constructor(private httpRequest: HttpRequestService) { }
+  constructor(private httpRequest: HttpRequestService) {
+  }
 
   public getAllProductsAndServices(): Observable<[ProductServiceResponse]> {
-    return this.httpRequest.request('/product-service');
+    if (this.enableMock) {
+      console.log("::: <MOCK>Enable!</MOCK> :::");
+      return of(productsAndServices);
+    } else {
+      return this.httpRequest.request('/product-service');
+    }
   }
 }
